174c9fca441947e041f0ff404b385073fbc5207d
coumbole <ville.kumpulainen@gmail.com>
2017-03-14 18:34:50 +0200

    Basic filtering working

---
+++ assets/js/filter.js
@@ -0,0 +1,27 @@
+function renderTableRow(name, email, keywords) {
+  var html = "<tr>";
+  html += "<td>" + name +  "</td>";
+  html += "<td>" + email + "</td>";
+  html += "<td>" + keywords + "</td>";
+  html += "</tr>";
+  return html;
+}
+
+window.onload = () => {
+
+  $.getJSON("emails.json", (json) => {
+    $.each(json, (index, value) => {
+      console.log(value);
+      $('#tablerows').append(renderTableRow("firstnamelastname", value.Email, "keywords"));
+    });
+  });
+
+  $("#contactinput").on('keyup', (e) => {
+    var value = e.target.value.toLowerCase();
+
+    $('#tablerows tr').each( (i, v) => {
+      $(v).toggle( $(v).text().toLowerCase().indexOf(value) !== -1 );
+    });
+  });
+
+};
